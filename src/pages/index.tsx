751e1c576b5509d12f6a64bf42793bcccc444b48
Vinicius Mazon <vnpmazon@gmail.com>
2021-02-23 15:03:02 -0300

    feat: add CompletedChallenges component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { Profile } from '../components/Profile';
 import ExperienceBar from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { CompletedChallenges } from '../components/CompletedChallenges';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -10,6 +11,7 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
         </div>
         <div></div>
       </section>
