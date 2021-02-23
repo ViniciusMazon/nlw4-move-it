2321ff91e5b50f5857ee0543497d9cfb7a45635e
Vinicius Mazon <vnpmazon@gmail.com>
2021-02-23 11:58:27 -0300

    chore: migrate components and styles to the next

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,17 @@
+export default function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+
+      <div>
+        <div style={{ width: '50%' }} />
+
+        <span className="current-experience" style={{ left: '50%' }}>
+          300 xp
+        </span>
+      </div>
+
+      <span>600 xp</span>
+    </header>
+  );
+}
