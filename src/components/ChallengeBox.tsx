d589fe90e8aaded5ea9232ff92e1f9e520848552
Vinicius Mazon <vnpmazon@gmail.com>
2021-02-24 16:46:28 -0300

    feat: add activeChallenge and resetChallenge feature to the ChallengeBox component

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,21 +1,34 @@
+import React from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = React.useContext(
+    ChallengesContext
+  );
 
   return (
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src="icons/body.svg" />
+            <img src={`icons/${activeChallenge.type}.svg`} />
+
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos</p>
+
+            <p>{activeChallenge.description}</p>
           </main>
+
           <footer>
-            <button type="button" className={styles.challengeFailButton}>
+            <button
+              type="button"
+              className={styles.challengeFailButton}
+              onClick={resetChallenge}
+            >
               Falhei
             </button>
             <button type="button" className={styles.challengeSucceededButton}>
