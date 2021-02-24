88f12525a5badfff914904a86f7cab3d88ca8cf9
Vinicius Mazon <vnpmazon@gmail.com>
2021-02-24 16:47:07 -0300

    feat: add challengesCompleted to the CompletedChallenges component

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges() {
+  const { challengesCompleted } = React.useContext(ChallengesContext);
+
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   );
 }
