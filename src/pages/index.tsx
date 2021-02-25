bf3134fdcaf03358b15ecef1ba147ddb6f9204ed
Vinicius Mazon <vnpmazon@gmail.com>
2021-02-25 16:22:02 -0300

    feat: add CountdownProvider to the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
-
-import ExperienceBar from '../components/ExperienceBar';
-import { Profile } from '../components/Profile';
+import React from 'react';
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
-import { ChallengeBox } from '../components/ChallengeBox';
-
+import ExperienceBar from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { CountdownProvider } from '../contexts/CountdownContext';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
@@ -15,16 +15,19 @@ export default function Home() {
         <title>Início | move.it</title>
       </Head>
       <ExperienceBar />
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   );
 }
